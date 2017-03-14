1f2773b14e470f93e5cd5fdaf6a23b6691428fc3
杜晓阳 <duxiaoyang521@live.cn>
2017-03-14 16:59:04 +0800

    ceshi

--- Doctors.API/Controllers/DoctorInforController.cs
+++ Doctors.API/Controllers/DoctorInforController.cs
@@ -32,6 +32,7 @@ namespace Doctors.API.Controllers
         [HttpGet]
         public IHttpActionResult Test()
         {
+            //duxiaoyang
             var item = doctorInforService.GetDoctorInfor("1");
             SimpleResult result = new SimpleResult();
             result.Resource = item;
